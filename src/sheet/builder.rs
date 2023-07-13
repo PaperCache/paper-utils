00c9358d3470e9fba1aa30cbd2478a14b9fb5f98
Kia Shakiba <kiashakiba@protonmail.com>
2023-07-13 16:55:30 -0400

    Remove references to small types

--- src/sheet/builder.rs
+++ src/sheet/builder.rs
@@ -12,59 +12,59 @@ impl SheetBuilder {
 		}
 	}
 
-	pub fn write_bool(mut self, value: &bool) -> Self {
-		let byte = if *value { b"!" } else { b"?" };
+	pub fn write_bool(mut self, value: bool) -> Self {
+		let byte = if value { b"!" } else { b"?" };
 		self.data.extend_from_slice(byte);
 		self
 	}
 
-	pub fn write_u8(mut self, value: &u8) -> Self {
-		self.data.push(*value);
+	pub fn write_u8(mut self, value: u8) -> Self {
+		self.data.push(value);
 		self
 	}
 
-	pub fn write_u16(mut self, value: &u16) -> Self {
+	pub fn write_u16(mut self, value: u16) -> Self {
 		let mut wtr = vec![];
-		wtr.write_u16::<LittleEndian>(*value).unwrap();
+		wtr.write_u16::<LittleEndian>(value).unwrap();
 
 		self.data.extend(wtr);
 		self
 	}
 
-	pub fn write_u32(mut self, value: &u32) -> Self {
+	pub fn write_u32(mut self, value: u32) -> Self {
 		let mut wtr = vec![];
-		wtr.write_u32::<LittleEndian>(*value).unwrap();
+		wtr.write_u32::<LittleEndian>(value).unwrap();
 
 		self.data.extend(wtr);
 		self
 	}
 
-	pub fn write_u64(mut self, value: &u64) -> Self {
+	pub fn write_u64(mut self, value: u64) -> Self {
 		let mut wtr = vec![];
-		wtr.write_u64::<LittleEndian>(*value).unwrap();
+		wtr.write_u64::<LittleEndian>(value).unwrap();
 
 		self.data.extend(wtr);
 		self
 	}
 
-	pub fn write_f32(mut self, value: &f32) -> Self {
+	pub fn write_f32(mut self, value: f32) -> Self {
 		let mut wtr = vec![];
-		wtr.write_f32::<LittleEndian>(*value).unwrap();
+		wtr.write_f32::<LittleEndian>(value).unwrap();
 
 		self.data.extend(wtr);
 		self
 	}
 
-	pub fn write_f64(mut self, value: &f64) -> Self {
+	pub fn write_f64(mut self, value: f64) -> Self {
 		let mut wtr = vec![];
-		wtr.write_f64::<LittleEndian>(*value).unwrap();
+		wtr.write_f64::<LittleEndian>(value).unwrap();
 
 		self.data.extend(wtr);
 		self
 	}
 
 	pub fn write_buf(mut self, value: &[u8]) -> Self {
-		self = self.write_u32(&(value.len() as u32));
+		self = self.write_u32(value.len() as u32);
 		self.data.extend(value);
 		self
 	}
