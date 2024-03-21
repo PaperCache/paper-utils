38f68a173934c95262675fc146713c75567c62e3
Kia Shakiba <kiashakiba@protonmail.com>
2024-03-21 04:52:27 -0400

    Add auth command byte + upgrade to v1.1.1

--- src/command.rs
+++ src/command.rs
@@ -4,19 +4,21 @@ impl CommandByte {
 	pub const PING: u8 = 0;
 	pub const VERSION: u8 = 1;
 
-	pub const GET: u8 = 2;
-	pub const SET: u8 = 3;
-	pub const DEL: u8 = 4;
+	pub const AUTH: u8 = 2;
 
-	pub const HAS: u8 = 5;
-	pub const PEEK: u8 = 6;
-	pub const TTL: u8 = 7;
-	pub const SIZE: u8 = 8;
+	pub const GET: u8 = 3;
+	pub const SET: u8 = 4;
+	pub const DEL: u8 = 5;
 
-	pub const WIPE: u8 = 9;
+	pub const HAS: u8 = 6;
+	pub const PEEK: u8 = 7;
+	pub const TTL: u8 = 8;
+	pub const SIZE: u8 = 9;
 
-	pub const RESIZE: u8 = 10;
-	pub const POLICY: u8 = 11;
+	pub const WIPE: u8 = 10;
 
-	pub const STATS: u8 = 12;
+	pub const RESIZE: u8 = 11;
+	pub const POLICY: u8 = 12;
+
+	pub const STATS: u8 = 13;
 }
